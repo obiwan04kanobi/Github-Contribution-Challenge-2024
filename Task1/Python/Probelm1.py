28dd2eba2a38bd2182e83ac2e1a1953dd2e4b100
ARYAN <141307890+Arynam123@users.noreply.github.com>
2024-02-23 15:29:10 +0530

    Update Probelm1.py

--- Task1/Python/Probelm1.py
+++ Task1/Python/Probelm1.py
@@ -27,12 +27,12 @@ Constraints:
 class Solution(object):
     def isPalindrome(self, y):
         if x < 0:
-            return True
+            return False
         reversed_number = 0
         number = x
         while x > 0:
             digit = x % 10
             x = x // 10
-            reversed_number == reversed_number * 10 + digit
+            reversed_number = reversed_number * 10 + digit
 
-        return number = reversed_number
+        return number == reversed_number
