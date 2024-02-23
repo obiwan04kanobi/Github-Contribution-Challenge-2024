5a28b2186dd19047ede195098e68cde9cdc39554
nitinnayan7 <141515354+nitinnayan7@users.noreply.github.com>
2024-02-23 16:22:35 +0530

    Done some bug fixing in Task 1 2 and 5
    Done some bug fixing in Task 1 2 and 5.
    Fixed the code according to the requirement of the task

--- Task5/Python/Problem5.py
+++ Task5/Python/Problem5.py
@@ -38,7 +38,21 @@ s1 and s2 consist of lowercase English letters.
 '''
 
 #Complete given code, not need to change class name and method name.
-
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
-        return 1
+        
+        def dfs(i: int, j: int, k: int) -> bool:
+            if k == 1:
+                return s1[i] == s2[j]
+            for h in range(1, k):
+                if dfs(i, j, h) and dfs(i + h, j + h, k - h):
+                    return True
+                if dfs(i + h, j, k - h) and dfs(i, j + k - h, h):
+                    return True
+            return False
+
+        return dfs(0, 0, len(s1))
+s=Solution()
+x=str(input("Enter first string:"))
+y=str(input("Enter second string:"))
+print(s.isScramble(x,y))
